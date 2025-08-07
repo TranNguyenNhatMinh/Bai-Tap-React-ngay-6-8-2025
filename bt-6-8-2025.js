fcfc5273476ad418963fb131220f2aaf652a12dc
TranNguyenNhatMinh <trannguyennhatminh8386@gmail.com>
2025-08-07 20:48:44 +0700

    Add files via upload

---
+++ bt-6-8-2025.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HW1 = (props) => {
+  return (
+    <div>
+      <h2>COMPONENT CON</h2>
+      <p>Ten nguoi dung: {props.userName}</p>
+      <p>COUNT: {props.count}</p>
+    </div>
+  )
+}
+
+export default HW1
